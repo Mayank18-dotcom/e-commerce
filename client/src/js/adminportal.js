a96ed4fad0639740456cf5b67de069c90589fcc3
Mayank18-dotcom <mayank123chittora@gmail.com>
2020-09-16 22:27:53 +0530

    admin-portal-updated

--- client/src/js/adminportal.js
+++ client/src/js/adminportal.js
@@ -2,3 +2,12 @@ $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+$(document).ready(function () {
+  $('.sec').hide();
+  $('#targetdiv1').show();
+  $('#selectMe').change(function () {
+    $('.sec').hide();
+    $('#'+$(this).val()).show();
+  })
+});
