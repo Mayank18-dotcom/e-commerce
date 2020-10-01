3e3dedf5f36549f470135128b45176f38639aef0
Mayank18-dotcom <mayank123chittora@gmail.com>
2020-10-01 21:29:43 +0530

    checkout added

---
+++ client/src/js/checkout.js
@@ -0,0 +1,6 @@
+$('#chadd').on('click', function() { 
+    $('#change-add').css('display', 'block'); 
+});
+$('#chadd1').on('click', function() { 
+    $('#change-add').css('display', 'none'); 
+});
