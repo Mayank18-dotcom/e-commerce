ee36b1ff20fae3c8d0e6dbfb8d3367f3944b8d83
Mayank18-dotcom <mayank123chittora@gmail.com>
2020-10-05 15:07:47 +0530

    coupons added

--- client/src/js/check-admin-login.js
+++ client/src/js/check-admin-login.js
@@ -1,36 +1,36 @@
-$.ajax({
-  url:
-    "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/admin/profile.php",
-  type: "GET",
-  xhrFields: {
-    withCredentials: true,
-  },
-  success: function (response, status, xhr) {
-    console.log("Admin is logged in");
-  },
-  error: function (xhr, status) {
-    console.log("error", xhr, status);
-    window.location.href =
-      "https://testing1.thestrategybook.com/deepak-ecommerce/client/adminportal/login.html";
-  },
-});
+// $.ajax({
+//   url:
+//     "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/admin/profile.php",
+//   type: "GET",
+//   xhrFields: {
+//     withCredentials: true,
+//   },
+//   success: function (response, status, xhr) {
+//     console.log("Admin is logged in");
+//   },
+//   error: function (xhr, status) {
+//     console.log("error", xhr, status);
+//     window.location.href =
+//       "https://testing1.thestrategybook.com/deepak-ecommerce/client/adminportal/login.html";
+//   },
+// });
 
-$(".logout-admin-btn").click(function (e) {
-  e.preventDefault();
-  var url =
-    "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/logout.php";
+// $(".logout-admin-btn").click(function (e) {
+//   e.preventDefault();
+//   var url =
+//     "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/logout.php";
 
-  $.ajax({
-    url: url,
-    type: "GET",
-    crossDomain: true,
-    success: function (response) {
-      window.location.href =
-        "https://testing1.thestrategybook.com/deepak-ecommerce/client/adminportal/login.html";
-    },
-    error: function (xhr, status) {
-      console.log("error", xhr, status);
-      alert("Some unknown error occured");
-    },
-  });
-});
+//   $.ajax({
+//     url: url,
+//     type: "GET",
+//     crossDomain: true,
+//     success: function (response) {
+//       window.location.href =
+//         "https://testing1.thestrategybook.com/deepak-ecommerce/client/adminportal/login.html";
+//     },
+//     error: function (xhr, status) {
+//       console.log("error", xhr, status);
+//       alert("Some unknown error occured");
+//     },
+//   });
+// });
